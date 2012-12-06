74ceb4a94a766a5d8f42d629a69237db0e537fca
Rahul Bhargava <rahulb@media.mit.edu>
2012-12-06 14:17:55 -0500

    add test-driven example

---
+++ electiondata.py
@@ -0,0 +1,18 @@
+class ElectionResults:
+  
+  def __init__(self, filename):
+    self.filename = filename
+
+  def load(self):
+    self.file = open(self.filename, 'r')
+    self.all_lines = self.file.readlines()
+
+  def states(self):
+    all_names = []
+    for line in self.all_lines:
+      columns = line.split(',')
+      all_names.append(columns[1])
+    return all_names
+
+  def state_count(self):
+    return len(self.all_lines)
