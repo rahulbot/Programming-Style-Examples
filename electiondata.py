dcddc39de32a27977368db6d8de26faf1f95f6d4
Rahul Bhargava <rahulb@media.mit.edu>
2013-11-14 10:45:14 -0500

    fix indentation (python standard is 4 spaces)

--- electiondata.py
+++ electiondata.py
@@ -1,18 +1,18 @@
 class ElectionResults:
   
-  def __init__(self, filename):
-    self.filename = filename
+    def __init__(self, filename):
+        self.filename = filename
 
-  def load(self):
-    self.file = open(self.filename, 'r')
-    self.all_lines = self.file.readlines()
+    def load(self):
+        self.file = open(self.filename, 'r')
+        self.all_lines = self.file.readlines()
 
-  def states(self):
-    all_names = []
-    for line in self.all_lines:
-      columns = line.split(',')
-      all_names.append(columns[1])
-    return all_names[1:]
+    def states(self):
+        all_names = []
+        for line in self.all_lines:
+            columns = line.split(',')
+            all_names.append(columns[1])
+        return all_names[1:]
 
-  def state_count(self):
-    return len(self.states())
+    def state_count(self):
+        return len(self.states())
